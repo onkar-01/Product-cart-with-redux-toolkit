e59dea9cf287e591208f1cad9512b58f6fd0a1b7
Onkar Vatsa <onkarvatsa@gmail.com>
2023-05-15 12:03:01 +0530

    image- correction

--- pages/components/ProductCard.jsx
+++ pages/components/ProductCard.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import Image from "next/image";
 const ProductCard = ({ name, id, price, handler, imgSrc }) => {
   return (
     <div className="productCard">
-      <img src={imgSrc} alt={name} />
+      <Image
+        loader={() => imgSrc}
+        src={imgSrc}
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
       <p>{name}</p>
       <h4>${price}</h4>
       <button onClick={() => handler({ id, name, price, imgSrc, quantity: 1 })}>
